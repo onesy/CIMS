fe0e005c0b22a45197eeff0a6065ef9dd8482948
onesy <leentingOne@gmail.com>
2013-02-04 15:48:49 +0800

    cfg bakup

---
+++ src/org/onesy/Module/CfgList.java
@@ -0,0 +1,72 @@
+package org.onesy.Module;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Properties;
+
+public class CfgList {
+	//配置文件基目录
+	public static String BasePath = null;
+	//根配置文件的目录的路径
+	public static String RootDirPath = null;
+	//根配置文件的路径
+	public static String RootCfg = null;
+	//本地cube配置文件目录路径
+	public static String LocalCubeDir = null;
+	//本地cube配置文件路径
+	public static String LocalCube = null;
+	//远程cube配置文件目录路径
+	public static String RemoteCubeDir = null;
+	//远程cube根配置文件路径
+	public static String Remotecube = null;
+	//远程cube支配置文件路径
+	public static ArrayList<String> RmtCfgList = new ArrayList<String>();
+	//plugin根配置文件目录路径
+	public static String PluginDir = null;
+	//pulgin根配置文件路径
+	public static String Plugin = null;
+	//pugins文件路径集合
+	public static ArrayList<String> PluginCfgList = new ArrayList<String>();
+	
+	/*
+	 * 配置文件加载顺序
+	 * 1.首先确认BasePath，读取根配置文件
+	 * 2.根据根配置文件读取到的
+	 */
+	
+	public CfgList(){
+		
+	}
+	
+	public boolean CfgLoad(){
+		
+		boolean okflg = true;
+		
+		//初始化根配置文件
+		BasePath = System.getProperty("user.home") + java.io.File.separator + ".cmcs";
+		RootDirPath = BasePath + java.io.File.separator + "Root";
+		RootCfg = RootDirPath + java.io.File.separator + "rootcfg.properties";
+		Properties rootCfg = new Properties();
+		try {
+			rootCfg.load(new FileInputStream(new File(RootCfg)));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			okflg = false;
+			e.printStackTrace();
+			return okflg;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			okflg = false;
+			return okflg;
+		}
+		
+		//初始化本地cube
+		
+		return okflg;
+	}
+
+}
